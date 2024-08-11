fb0f6d0a46cb8a1129a9c0eea72646a0806a61c2
Afnan <afnan@google.com>
2024-08-11 11:48:07 +0800

    changed fizz to fuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 
 for num in range(1, 101):
     if num % 15 == 0:
-        print("FizzBuzz")
+        print("FuzzBuzz")
     elif num % 3 == 0:
-        print("Fizz")
+        print("Fuzz")
     elif num % 5 == 0:
         print("Buzz")
     else:
